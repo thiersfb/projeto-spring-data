049c2d3ab72f3dfac2d75e1135fe55099b6ef7a2
thiersfb <thiers.fb@gmail.com>
2024-08-20 18:06:35 -0300

    Realizando cadastros no banco de dados

--- projeto-spring-data-aula/src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
+++ projeto-spring-data-aula/src/main/java/projeto/spring/data/aula/model/UsuarioSpringData.java
@@ -18,11 +18,13 @@ import javax.persistence.Table;
 public class UsuarioSpringData {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String login;
 	private String senha;
 	private String nome;
+	private String email;
 	private int idade;
 	public Long getId() {
 		return id;
@@ -47,6 +49,13 @@ public class UsuarioSpringData {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	public int getIdade() {
 		return idade;
--- projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ projeto-spring-data-aula/src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import projeto.spring.data.aula.dao.InterfaceSpringDataUser;
+import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
@@ -17,6 +18,21 @@ public class AppSpringDataTest {
 	
 	@Test
 	public void testeInsert() {
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setEmail("thiers.fb@gmail.com");
+		usuarioSpringData.setIdade(36);
+		usuarioSpringData.setLogin("thiersfb");
+		usuarioSpringData.setSenha("thiers07");
+		usuarioSpringData.setNome("Thiers Barizon");
+		
+		interfaceSpringDataUser.save(usuarioSpringData);
+		
+		System.out.println("Qtd Usuarios cadastrados no banco: " + interfaceSpringDataUser.count());
+		
+	}
+	
+	@Test
+	public void testeConsulta() {
 		System.out.println("Iniciou spring com sucesso");
 	}
 	
